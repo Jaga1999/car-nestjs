d43f8164448b81b52f30aa8f4c258dbb1f4ef27f
Jaga1999 <jagasiva1999@gmail.com>
2023-08-15 13:36:19 +0530

    created all service functionalities

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './users.entity';
+import { error } from 'console';
 
 @Injectable()
 export class UsersService {
@@ -9,9 +10,33 @@ export class UsersService {
     @InjectRepository(User) private readonly repo: Repository<User>,
   ) {}
 
-  create(email: string, password: string) {
-    const user = this.repo.create({ email, password });
-
+  async create(email: string, password: string) {
+    const user = await this.repo.create({ email, password });
     return this.repo.save(user);
+  }
+
+  async findOne(id: number) {
+    return await this.repo.findOneBy({ id });
+  }
+
+  async find(email: string) {
+    return await this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new error('User Not Found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new error('User Not Found');
+    }
+    return this.repo.remove(user);
   }
 }
