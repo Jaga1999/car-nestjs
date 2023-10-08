9afe9084dd3348e7e6fb6fb6ca1995efb1176e25
Jagamantra <jagadeep@mantracollab.com>
2023-10-08 16:01:31 +0530

    first test file cleanup

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,22 +2,6 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 
-// describe('AuthService', () => {
-//   let service: AuthService;
-
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [AuthService],
-//     }).compile();
-
-//     service = module.get<AuthService>(AuthService);
-//   });
-
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
-
 it('Create an Instance of Auth Serice', async () => {
   // Fake User Service for testing
   const fakeUserService = {
