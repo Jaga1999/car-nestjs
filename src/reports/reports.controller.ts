5b5f40546b1e0133dfc6cd96765c16078b51573c
Jagamantra <jagadeep@mantracollab.com>
2023-10-08 20:25:10 +0530

    made some good progress in report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { CreateReportDto } from './DTO/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ReportDto } from './DTO/report.dto';
+import { ApproveReportDto } from './DTO/approve-report.dto';
 
+@UseGuards(AuthGuard)
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportSerice: ReportsService) {}
+
+  @Post()
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user) {
+    return this.reportSerice.create(body, user);
+  }
+
+  @Get()
+  getAll() {
+    return this.reportSerice.getAll();
+  }
+
+  @Patch('/:id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportSerice.changeApproval(id, body.approved);
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateReportDto } from './DTO/create-report.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './reports.entity';
+import { User } from 'src/users/users.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report) private readonly repo: Repository<Report>,
+  ) {}
+
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.repo.findOne({ where: { id: parseInt(id) } });
+    if (!report) {
+      throw new NotFoundException('Report not found!.');
+    }
+    report.approved = approved;
+    return this.repo.save(report);
+  }
+
+  getAll() {
+    return this.repo.find();
+  }
+
+  create(reportDto: CreateReportDto, user: User) {
+    const report = this.repo.create(reportDto);
+    report.user = user;
+    return this.repo.save(report);
+  }
+}
