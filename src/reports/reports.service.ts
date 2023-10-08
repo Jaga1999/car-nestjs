4a103d7d4cc1b0b6e974dc8a8b743e00a0437104
Jagamantra <jagadeep@mantracollab.com>
2023-10-08 21:19:25 +0530

    finished estimated price calculation

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './reports.entity';
 import { User } from 'src/users/users.entity';
+import { GetEstimateDto } from './DTO/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
@@ -18,6 +19,22 @@ export class ReportsService {
     }
     report.approved = approved;
     return this.repo.save(report);
+  }
+
+  createEstimate(estimatedto: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make: estimatedto.make })
+      .andWhere('model = :model', { model: estimatedto.model })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat: estimatedto.lat })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng: estimatedto.lng })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year: estimatedto.year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(milage - :milage)', 'DESC')
+      .setParameters({ milage: estimatedto.milage })
+      .limit(3)
+      .getRawOne();
   }
 
   getAll() {
